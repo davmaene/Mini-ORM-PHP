872a6953ff6af6f5e7eb0891267fc59f004f65c4
davmaene <davidmened@gmail.com>
2021-06-29 20:01:56 +0200

    up logfile

--- pont.php
+++ pont.php
@@ -1,14 +1,14 @@
 <?php 
-// ---------------------------------
+// -------------------------------------------
 // ----- dont delete this file -----
-// ---------------------------------
+// -------------------------------------------
 include_once("./ware/model.log.php");
-// --------- interfaces -------------
+// --------- interfaces ----------------------
 include_once("./ware/interface.init.php");
 include_once("./ware/interface.metier.php");
-// ----------------------------------
+// -------------------------------------------
 include_once("./ware/model.config.php");
 include_once("./ware/model.crud.php");
-// ----------- main ------------------
+// ----------- main --------------------------
 require("./main/main.php");
 ?>
