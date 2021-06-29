ff6d7c8cb9bfb07890d3ece098ea137d537d313f
davmaene <davidmened@gmail.com>
2021-06-29 20:23:50 +0200

    up line

--- ware/model.response.php
+++ ware/model.response.php
@@ -5,7 +5,13 @@ class Response{
     public $statusText;
     public $body;
 
-    public function __construct(){}
-    
+    public function __construct($status, $statusText, $body){
+        $this->status = $status;
+        $this->statusText = $statusText;
+        $this->body = $body;
+    }
+    public function getStatus(){return $this->status;}
+    public function getstatusText(){return $this->statusText;}
+    public function getBody(){return $this->body;}
 }
 ?>
