d0b66635ad89e44d31c5236647bacda1a6d7b5e2
davmaene <davidmened@gmail.com>
2021-07-01 20:06:48 +0200

    up line:

--- ware/model.crud.php
+++ ware/model.crud.php
@@ -4,18 +4,26 @@
         public function __construct(){
 
         }
+        private function __SQLCreateInstance($tablename, $properties){
+            $createTable = "CREATE TABLE 
+            `_tbl_test` ( `id` INT NOT NULL AUTO_INCREMENT , 
+            `fsname` VARCHAR(60) NOT NULL , 
+            `lsname` VARCHAR(60) NOT NULL , 
+            `country` VARCHAR(60) NOT NULL , 
+            PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+        } 
         protected function __creteClass(){
             $prefixe = "__tbl_";
             $classname = get_class($this);
             if(!empty($classname)){
               if(!strpos($classname,"s",-1)) $classname .= "s";
               $classname = strtolower($prefixe.$classname);
-              echo($classname);
+            //   echo($classname);
             }else return new Response(500, ["dav.me i can't get the className property"]);
         }
         protected function declencher(){
             $conf = new Config();
-            return !$conf->__inst() ? new Response(202, []) : new Response(500, []);
+            return $conf->onInit() ? new Response(202, []) : new Response(500, []);
         }
         public function onAdd(){
             $this->__creteClass();
