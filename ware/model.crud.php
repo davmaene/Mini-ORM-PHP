54258b71203184d0f4041bfbf473fd2fff6ef29e
davmaene <davidmened@gmail.com>
2021-07-01 18:48:18 +0200

    up line

--- ware/model.crud.php
+++ ware/model.crud.php
@@ -5,18 +5,20 @@
 
         }
         protected function __creteClass(){
+            $prefixe = "__tbl_";
             $classname = get_class($this);
             if(!empty($classname)){
-              if(strpos($classname,"s",-1)){
-                  echo("found s");
-              }
-            }else return new Response(500, ["dav.me i can't get the className sorry"]);
+              if(!strpos($classname,"s",-1)) $classname .= "s";
+              $classname = strtolower($prefixe.$classname);
+              echo($classname);
+            }else return new Response(500, ["dav.me i can't get the className property"]);
         }
         protected function declencher(){
             $conf = new Config();
             return !$conf->__inst() ? new Response(202, []) : new Response(500, []);
         }
         public function onAdd(){
+            $this->__creteClass();
             if(!is_object($this)) return new Response(401,[]);
             else{
                 $inst = $this;
