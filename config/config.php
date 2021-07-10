68c87dacf0e92a0a387f462ae801f82db74d9023
davmaene <davidmened@gmail.com>
2021-07-10 17:13:58 +0300

    up line

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php 
 // custermer enviroment infos goes here
 $customer_dbname = "_dbmidleware"; // the name of your database
-$customer_dialect = "mysql"; // env cloud
+$customer_dialect = "mysql"; // env database service cloud
 $customer_hostname = "localhost"; // name or ip of host
 $customer_username = "root"; // username to access to db
 $customer_password = ""; // password to access to db
