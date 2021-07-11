ba5aab4dc67e142c4ebf96860a5a78769f0d0f1e
davmaene <davidmened@gmail.com>
2021-07-11 08:59:29 +0300

    up line

--- config/config.php
+++ config/config.php
@@ -5,8 +5,9 @@ $customer_dialect = "mysql"; // env database service cloud
 $customer_hostname = "localhost"; // name or ip of host
 $customer_username = "root"; // username to access to db
 $customer_password = ""; // password to access to db
-// dont modify code beyond this line
-// ---------------------------------
+// ---------------------------------------------------------
+//          dont modify code beyond this line
+// ---------------------------------------------------------
 define("env", // environement
      array(
          "dialect" => $customer_dialect ?? "mysql",
